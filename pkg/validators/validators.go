639a5417f380b6d8328709c9fe20850cb058a39f
Saara Tyagi <saaratyagi@google.com>
2026-02-17 05:56:28 +0000

    cleanup

--- pkg/validators/validators.go
+++ pkg/validators/validators.go
@@ -255,7 +255,7 @@ func defaults(bp config.Blueprint) []config.Validator {
 		})
 		for _, varName := range bp.Vars.Keys() {
 			// Do not run logic for future reservations
-			if resKeyRegex.MatchString(varName) && !strings.Contains(varName, "future") {
+			if resKeyRegex.MatchString(varName) {
 				resRef := config.GlobalRef(varName).AsValue()
 
 				// Automatically add a reservation check for every detected reservation variable
